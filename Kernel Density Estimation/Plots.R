037b17d583d4e0c88687d2cfc4ed8d0070a4dc3d
GaspardLeMarque <gleb.ushakov@yahoo.com>
2021-04-20 21:10:18 +0200

    Add a boxplot and a benchmark section

--- Kernel Density Estimation/Plots.R
+++ Kernel Density Estimation/Plots.R
@@ -22,3 +22,60 @@ load("C:/Users/User 1/Desktop/t_KNN.RDA")
 load("C:/Users/User 1/Desktop/bimodal_KNN.RDA")
 load("C:/Users/User 1/Desktop/bimodal_AKDE.RDA")
 load("C:/Users/User 1/Desktop/t_AKDE.RDA")
+
+IMSE <- rbind(
+  data.frame(variable = "standard_normal", value = IMSE_normal, method = "Plug-in bandwidth"),
+  data.frame(variable = "standard_normal", value = IMSE_normal_silver, method = "Silvermans rule"),
+  data.frame(variable = "standard_normal", value = IMSE_normal_AKDE, method ="Adaptive KDE"),  
+  data.frame(variable = "standard_normal", value = IMSE_standard_normal_KNN, method = "kNN"),
+  data.frame(variable = "t_distribution", value = IMSE_t_dist, method = "Plug-in bandwidth"),
+  data.frame(variable = "t_distribution", value = IMSE_silver_t_dist, method = "Silvermans rule"),
+  data.frame(variable = "t_distribution", value = IMSE_t_AKDE, method = "Adaptive KDE"),
+  data.frame(variable = "t_distribution", value = IMSE_t_KNN, method = "kNN"),
+  data.frame(variable = "bimodal_normal", value = IMSE_bimodal_normal, method = "Plug-in bandwidth"),
+  data.frame(variable = "bimodal_normal", value = IMSE_bimodal_normal_silver, method = "Silvermans rule"),
+  data.frame(variable = "bimodal_normal", value = IMSE_bimodal_AKDE, method = "Adaptive KDE"),
+  data.frame(variable = "bimodal_normal", value = IMSE_bimodal_KNN, method = "kNN"),
+  data.frame(variable = "beta_gamma", value = IMSE_gamma_beta, method = "Plug-in bandwidth"),
+  data.frame(variable = "beta_gamma", value = IMSE_gamma_beta_silver, method = "Silvermans rule"),
+  data.frame(variable = "beta_gamma", value = IMSE_gamma_beta_AKDE, method = "Adaptive KDE"),
+  data.frame(variable = "beta_gamma", value = IMSE_gamma_beta_KNN, method = "kNN")
+)
+IMSE$variable <- factor(x = IMSE$variable, levels = c("standard_normal", "t_distribution", "bimodal_normal", "beta_gamma"))
+IMSE$method <- factor(x = IMSE$method, levels = c("Plug-in bandwidth", "Silvermans rule", "Adaptive KDE", "kNN"))
+
+#Boxplot
+plot_ly(data = IMSE, x = ~variable, y = ~value, color = ~method, type = "box") %>%
+  layout(boxmode = "group", 
+         xaxis = list(title="", 
+                      showline = FALSE,
+                      showticklabels = FALSE,
+                      showgrid = FALSE), 
+         yaxis = list(title="MISE", titlefont = list(size = 20), tickfont = list(size = 20)),
+         annotations = list(x=c(0, 1, 2, 3), 
+                            y = c(0.22, 0.22, 0.22, 0.22), 
+                            text=sprintf("<b>%s</b>",c("Standard\nNormal", "Bimodal\nNormal", "t-Distribution", "Beta-Gamma", "")),
+                            showarrow = FALSE,
+                            font = list(size = 20)),
+         legend = list(font=list(size=20), x= 0, y=0.85)) #change fontsize (probably 20)
+
+#Benchmark
+mean(extract_numeric(log.txt_bimodal_AKDE))
+mean(extract_numeric(log.txt_bimodal_KNN))
+mean(extract_numeric(log.txt_bimodal))
+mean(extract_numeric(log.txt_bimodal_sil))
+
+mean(extract_numeric(log.txt_gamma_beta_AKDE))
+mean(extract_numeric(log.txt_gamma_beta_KNN))
+mean(extract_numeric(log.txt_gamma_beta))
+mean(extract_numeric(log.txt_gamma_beta_sil))
+
+mean(extract_numeric(log.txt_normal_AKDE))
+mean(extract_numeric(log.txt_standard_normal_KNN))
+mean(extract_numeric(log.txt_normal))
+mean(extract_numeric(log.txt_normal_silver))
+
+mean(extract_numeric(log.txt_t_AKDE))
+mean(extract_numeric(log.txt_t_KNN))
+mean(extract_numeric(log.txt_t))
+mean(extract_numeric(log.txt_t_silver))
